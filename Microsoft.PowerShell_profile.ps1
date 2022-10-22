8febf3dbde8629625af36dc32e88f8e0e7c1b37e
Dragon-GCS <878173121@qq.com>
2022-10-22 14:43:27 +0800

    add yapf config

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,4 @@
 # set envirnment variable
-
 $ENV:XDG_CONFIG_HOME = "$HOME\.config"
 $ENV:XDG_DATA_HOME = $HOME
 Set-Alias vim nvim
@@ -9,10 +8,6 @@ Invoke-Expression (&starship init powershell)
 
 # set rustup autocompletion
 Invoke-Expression (&rustup completions powershell | Out-String)
-
-# add script path
-$ENV:PATH += ";E:\ProjectFiles\Python\Mine\"
-$ENV:PATH += ";D:\neovim\bin\"
 
 # add py to executable file
 $ENV:PATHEXT += ";.PY"
